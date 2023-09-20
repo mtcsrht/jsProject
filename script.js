d79d7a0ab7cab70701993266a8341a49892557b2
Mate <mate@localhost.hu>
2023-09-20 13:23:52 +0200

    jko

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let button = document.getElementById('random');
 let box = document.getElementById('box');
+let bigbox = document.getElementById('bigbox');
+
+
 function randomcolor()
 {
     let r = Math.floor(Math.random()*256);
     let g = Math.floor(Math.random()*256);
     let b = Math.floor(Math.random()*256);
-    let w = Math.floor(Math.random()*50);
-    let h = Math.floor(Math.random()*50);
+    let w = Math.floor(Math.random()*1000);
+    let h = Math.floor(Math.random()*700);
     box.style.backgroundColor = "rgb("+r+","+g+","+b+")";
-    box.style.width = w+"em";
-    box.style.height = h+"em";
+    box.style.width = w+"px";
+    box.style.height = h+"px";
+    
 }
+
 button.onclick = function(){
     randomcolor();
 }
